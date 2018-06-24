959441a63d27a1e35a1fb55a6d72aef2c1ce398a
Víctor <victormanu_96@hotmail.com>
2018-06-24 18:56:59 +0200

    ParallaxScrolling
    Incluidas 3 capas de background realizando parallax scrolling y primer commit

---
+++ S-PAC-3 MAN/Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour {
+	
+	public float scrollDebuff;		/* Parallax Scrolling */
+	private MeshRenderer mr;
+	void Start(){
+		
+		mr = GetComponent<MeshRenderer>();
+
+	}
+	void Update () {
+		
+		mr.material.SetTextureOffset("_MainTex",(transform.position / transform.localScale.x)/scrollDebuff );
+		
+	}
+}
