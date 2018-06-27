b270b0329d890e76b5b4b82979152388cff4ce3e
Víctor <victormanu_96@hotmail.com>
2018-06-27 13:19:53 +0200

    Demo
    Situado un texto gameOver al perder. Vinculado a PlayerCollision su setActive.

--- S-PAC-3 MAN/Assets/Scripts/PlayerCollision.cs
+++ S-PAC-3 MAN/Assets/Scripts/PlayerCollision.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
 
-
+	[SerializeField] private GameObject gameOver;
+	private Animator anim;
+	private bool dead;
+	void Start(){
+		anim = GetComponent<Animator>();
+		anim.SetBool("isDead",false);
+		
+		gameOver.SetActive(false);
+	}
 	void OnTriggerEnter2D(Collider2D other){
 		
 		if(other.CompareTag("Item")) {
 			Destroy(other.gameObject);
 		}else{
 			if(other.CompareTag("Enemy")) {
-				Destroy(gameObject);
+				GetComponent<PlayerController>().isDead = true;
+				anim.SetBool("isDead",true);
+
+				gameOver.SetActive(true);
 			}
 		}
 	}
--- S-PAC-3 MAN/Assets/Scripts/PlayerController.cs
+++ S-PAC-3 MAN/Assets/Scripts/PlayerController.cs
@@ -6,22 +6,26 @@ public class PlayerController : MonoBehaviour {
 
 	[SerializeField] private float movementSpeed;
 	[SerializeField] private float rotationSpeed;
+	public bool isDead;
 	private float ScreenWidth;
 
 	void Start(){
 		ScreenWidth = Screen.width;
+		isDead = false;
 	}
 
 	void Update () {
 		int direction = 0;
-		if (Input.touchCount > 0){
-			if (Input.GetTouch (0).position.x > ScreenWidth / 2) {					//Move Right
-				direction = -1;
-			}else{																	//Move Left
-				direction = 1;
+		if(isDead == false){		// Quiero que al morir siga hacia delante pero ya no se pueda controlar
+			if (Input.touchCount > 0){
+				if (Input.GetTouch (0).position.x > ScreenWidth / 2) {					//Move Right
+					direction = -1;
+				}else{																	//Move Left
+					direction = 1;
+				}
 			}
 		}
-		//transform.Rotate(new Vector3(0,0,-Input.GetAxis("Horizontal") * rotationSpeed));		Para probar en PC
+		//transform.Rotate(new Vector3(0,0,-Input.GetAxis("Horizontal") * rotationSpeed));		//Para probar en PC
 		transform.Rotate(new Vector3(0,0,direction * rotationSpeed));
 		transform.position += transform.up * Time.deltaTime * movementSpeed;
 	}
