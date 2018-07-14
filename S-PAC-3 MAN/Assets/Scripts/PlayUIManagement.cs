b98bca845b329b1eaf1ac55d8d95e24f88d355f9
Víctor <victormanu_96@hotmail.com>
2018-07-14 20:10:02 +0200

    Añadido temporizador.
    Añadido temporizador
    Modificada puntuación

--- S-PAC-3 MAN/Assets/Scripts/PlayUIManagement.cs
+++ S-PAC-3 MAN/Assets/Scripts/PlayUIManagement.cs
@@ -12,18 +12,29 @@ public class PlayUIManagement : MonoBehaviour {
 	[SerializeField] private GameObject quitBt;
 	[SerializeField] private GameObject player;
 	[SerializeField] private Text counterText;
+	[SerializeField] private Text timerText;
 	private PlayerController playerScript;
 	private PauseController pauseScript;
+	private float time;
+	private int timerMinutes;
+	private int timerSeconds;
 	void Start () {
 		gameOverText.SetActive(false);
 		pauseText.SetActive(false);
 		ActiveBts(false);
+
 		playerScript = player.GetComponent<PlayerController>();
 		pauseScript = this.gameObject.GetComponent<PauseController>();
+
+		time = 0;
 	}
 	
 	void Update(){
+
+		SetTimer();
+
 		counterText.text = playerScript.GetPickUps().ToString();
+		
 		if(playerScript.GetDeath()){
 			pauseBt.SetActive(false);
 			gameOverText.SetActive(true);
@@ -37,6 +48,15 @@ public class PlayUIManagement : MonoBehaviour {
 		}
 	}
 
+	void SetTimer(){
+		if(playerScript.GetDeath() == false){
+			time += Time.deltaTime;
+			timerSeconds = (int)Mathf.Floor(time);
+			timerMinutes = timerSeconds / 60;
+
+			timerText.text = string.Format("{0:D2}:{1:D2}", timerMinutes, timerSeconds % 60);
+		}
+	}
 	void ActiveBts(bool boolean){
 		restartBt.SetActive(boolean);
 		menuBt.SetActive(boolean);
